d4c959243b6879a34503577c440bb8038af385a9
Danielle <danimeye@umich.edu>
2018-09-24 09:53:44 -0400

    working on 1.4

--- challenges.py
+++ challenges.py
@@ -1,4 +1,10 @@
-#import statements go here 
+from flask import Flask, request, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import Required, Email
+
+import requests
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -7,20 +13,41 @@ app.debug = True
 @app.route('/')
 def home():
     return "Hello, world!"
+
+class itunesForm(FlaskForm):
+	artist = StringField('Enter an artist name.', validators=[Required()])
+	email = StringField('Enter an email.', validators=[Required(), Email()])
+	results = IntegerField('Enter the number of results.',validators = [Required()])
+	submit = SubmitField('Submit')
     
 #create class to represent WTForm that inherits flask form
 
 @app.route('/itunes-form')
 def itunes_form():
-    #what code goes here?
-    return render_template('itunes-form.html', form=simpleForm) # HINT : create itunes-form.html to represent the form defined in your class
+	simpleForm = itunesForm()
+	return render_template('itunes-form.html', form=simpleForm) # HINT : create itunes-form.html to represent the form defined in your class
 
 @app.route('/itunes-result', methods = ['GET', 'POST'])
 def itunes_result():
-    #what code goes here?
-    # HINT : create itunes-results.html to represent the results and return it
-    flash('All fields are required!')
-    return redirect(url_for('itunes_form')) #this redirects you to itunes_form if there are errors
+	form = itunesForm(request.form)
+	if request.method == 'POST' and form.validate_on_submit():
+	    artist = form.artist.data # returns a dictionary
+	    email = form.email.data
+	    results = form.results.data
+
+	    base_url = "https://itunes.apple.com/search"
+	    params_diction = {}
+	    params_diction["term"] = artist
+	    params_diction["country"] = 'US'
+	    params_diction["limit"] = results 
+	    resp = requests.get(base_url, params = params_diction)
+	    response_py = json.loads(resp.text)['results']
+	flash('All fields are required!')
+	return render_template('itunes-results.html', result_html = response_py)
+
+    #HINT : create itunes-results.html to represent the results and return it
+
+    #return redirect(url_for('itunes_form')) #this redirects you to itunes_form if there are errors
 
 if __name__ == '__main__':
     app.run()
